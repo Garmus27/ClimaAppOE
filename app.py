d495a01f1451e06283190c02ebef9ff476e9eba0
juan martin bravo <summun1138@gmail.com>
2024-10-18 00:58:35 -0300

    Anadi un loop para volver a preguntar si se desea ingresar otra ciudad

--- app.py
+++ app.py
@@ -47,49 +47,81 @@ def settings_menu():
 # Ejecución de la aplicación
 settings_menu()
 
+# iteracion para volver a ejecutar el loop
+def pregunta(valor2):
+
+    seguir = "s"
+    salir = "n"
+    
+
+    while True:
+        print("desea volver a ingresar ciudad, s/n?")
+        v = input().lower() # para que no distinga entre mayusculas y minusculas
+        
+        if v == seguir:
+            valor2 = False
+            
+            break
+        if v == salir :
+            valor2 = True
+
+            break
+
+    return valor2
+
+
 import requests
 
 api_key = "ff665520476ccad9dc27238103288d2c"
 
 base_url = "https://api.openweathermap.org/data/2.5/weather?"
 
-city_name = input("Ingrese una ciudad: ")
 
-url = base_url + "q=" + city_name + "&appid=" + api_key
+valor = False
 
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # Esto genera un error para códigos de estado HTTP 4xx o 5xx
 
-    data = response.json()
+while valor == False: # mientras el valor obtenido de la funcion pregunta() sea falso seguira preguntando
 
-    if data["cod"] != "404":
-        # Extraer el bloque principal de información
-        main = data["main"]
-        # Extraer la temperatura
-        temperatura = main["temp"]
-        # Extraer la presión
-        presion = main["pressure"]
-        # Extraer la humedad
-        humedad = main["humidity"]
+
+    city_name = input("Ingrese una ciudad: ")
+
+    url = base_url + "q=" + city_name + "&appid=" + api_key
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Esto genera un error para códigos de estado HTTP 4xx o 5xx
+
+        data = response.json()
+
+        if data["cod"] != "404":
+            # Extraer el bloque principal de información
+            main = data["main"]
+            # Extraer la temperatura
+            temperatura = main["temp"]
+            # Extraer la presión
+            presion = main["pressure"]
+            # Extraer la humedad
+            humedad = main["humidity"]
+            
+            # Extraer la descripción del clima
+            descripcion = data["weather"][0]["description"]
+            
+            # Imprimir los resultados
+            print(f"Temperatura: {temperatura} K")
+            print(f"Presión: {presion} hPa")
+            print(f"Humedad: {humedad}%")
+            print(f"Descripción del clima: {descripcion}")
         
-        # Extraer la descripción del clima
-        descripcion = data["weather"][0]["description"]
+            valor = pregunta(valor)
+
         
-        # Imprimir los resultados
-        print(f"Temperatura: {temperatura} K")
-        print(f"Presión: {presion} hPa")
-        print(f"Humedad: {humedad}%")
-        print(f"Descripción del clima: {descripcion}")
-    else:
-        print({"codigo_error": 404, "mensaje": "Ciudad no encontrada."})
 
-except requests.exceptions.ConnectionError:
-    print({"codigo_error": 100, "mensaje": "Error de conexión. Verifique su conexión a Internet."})
-
-except requests.exceptions.Timeout:
-    print({"codigo_error": 101, "mensaje": "La solicitud excedió el tiempo de espera."})
-
-except requests.exceptions.RequestException as e:
+    except requests.exceptions.ConnectionError:
+        print({"codigo_error": 100, "mensaje": "Error de conexión. Verifique su conexión a Internet."})
+        valor = pregunta(valor)
+    except requests.exceptions.Timeout:
+        print({"codigo_error": 101, "mensaje": "La solicitud excedió el tiempo de espera."})
+        valor = pregunta(valor)
+    except requests.exceptions.RequestException as e:
     # Cualquier otro tipo de error relacionado con la solicitud
-    print({"codigo_error": 102, "mensaje": f"Error al realizar la solicitud: {str(e)}"})
+        print({"codigo_error": 102, "mensaje": f"Error al realizar la solicitud: {str(e)}"})
+        valor = pregunta(valor)
